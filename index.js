9636679737792f12485d3c7dab4444641e8e12a2
Nicole white <you@example.com>
2020-02-13 09:21:41 -1000

    dep

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-require('dotenv').config();
+// require('dotenv').config();
 
 const Users = require('./data/db.js');//find(),findById(id),insert(),update(id(of user),changes(to apply)),remove(id)
 
